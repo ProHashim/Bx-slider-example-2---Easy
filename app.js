4b79d9174575d752dee37b84ae637bf5d46af79d
Muhammad Hashim <56976652+ProHashim@users.noreply.github.com>
2021-08-18 16:43:39 +0500

    simple slider using BxSlider

---
+++ app.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $(".slider").bxSlider({
+        mode: 'vertical', //option fade, vertical, horizontol
+        captios: true,
+        slideWidth: 600,
+        auto: true,
+        controls: true //true by default
+
+
+
+    })
+});
